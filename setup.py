d65e3d2bd245d4055652768091c91551fdd031a1
李育論 <yulun@yara.lu.im.ntu.edu.tw>
2017-09-16 23:13:02 +0800

    123

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 setup(
   name='namesex',
   packages = ['namesex'],
-  version = '2.2', 
+  version = '2.3', 
   author = 'HSIN-MIN LU, YU-LUN LI, CHI-YU LIN',
   author_email = 'yulun0528@gmail.com',
   keywords = ['name,gender'],
   url = 'https://github.com/yulun0528/namesex',
-  download_url = 'https://github.com/yulun0528/namesex/archive/2.2.tar.gz'
+  download_url = 'https://github.com/yulun0528/namesex/archive/2.3'
 )
