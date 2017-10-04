6e21bfd44f1ef3b7dee3137fc07956be5284f3e4
EvanYo25 <b04705023@ntu.edu.tw>
2017-10-04 21:02:32 +0800

    jkl

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   packages = ['namesex'],
   version = '1.0', 
   author = 'HSIN-MIN LU, YU-LUN LI, CHI-YU LIN',
-  author_email = 'yulun0528@gmail.com',
+  author_email = 'evan860126@gmail.com',
   keywords = ['name,gender'],
   url = 'https://github.com/EvanYo25/namesex-1',
   download_url = 'https://github.com/EvanYo25/namesex-1/tarball/v1.0'
