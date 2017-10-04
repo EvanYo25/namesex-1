b3b88bd0f9f99776f3921206552d2b8032942726
EvanYo25 <b04705023@ntu.edu.tw>
2017-10-04 20:57:30 +0800

    jkl

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup
 setup(
   name='namesex',
   packages = ['namesex'],
-  version = '2.5', 
+  version = '1.0', 
   author = 'HSIN-MIN LU, YU-LUN LI, CHI-YU LIN',
   author_email = 'yulun0528@gmail.com',
   keywords = ['name,gender'],
-  url = 'https://github.com/yulun0528/namesex',
-  download_url = 'https://github.com/yulun0528/namesex/archive/2.5.tar.gz'
+  url = 'https://github.com/EvanYo25/namesex-1',
+  download_url = 'https://github.com/EvanYo25/namesex-1/tarball/v1.0'
 )
